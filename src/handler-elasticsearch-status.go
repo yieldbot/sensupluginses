d22e251d3c2b9f2b07bb4e092679c1a76d645d58
Matt Jones <mattjones@yieldbot.com>
2015-11-20 09:22:04 -0500

    fix formatting issues

--- src/handler-elasticsearch-status.go
+++ src/handler-elasticsearch-status.go
@@ -76,10 +76,10 @@ func main() {
 
 	// Check to see if the index exists and if not create it
 	if client.IndexExists(esIndex) == nil { // need to test to make sure this does what I want
-  	_, err = client.CreateIndex(esIndex).Do()
-  	if err != nil {
-  		dracky.Check(err)
-  	}
+		_, err = client.CreateIndex(esIndex).Do()
+		if err != nil {
+			dracky.Check(err)
+		}
 	}
 
 	// Create an Elasticsearch document. The document type will define the mapping used for the document.
